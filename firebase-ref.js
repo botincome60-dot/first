2971bacafe2b5387194948fd4969966c2697bb00
botincome60-dot <botincome60@gmail.com>
2025-11-25 12:42:17 +0600

    sdkjfhdsikfhiuhds

--- firebase-ref.js
+++ firebase-ref.js
@@ -1,12 +1,12 @@
 // Firebase initialization
 const firebaseConfig = {
-    apiKey: "AIzaSyABdp9WK7eGLwE5nY19jp-nlDlyTuTyMR0",
-  authDomain: "sohojincome-36f1f.firebaseapp.com",
-  projectId: "sohojincome-36f1f",
-  storageBucket: "sohojincome-36f1f.firebasestorage.app",
-  messagingSenderId: "398153090805",
-  appId: "1:398153090805:web:fc8d68130afbc2239be7bc",
-  measurementId: "G-VZ47961SJV"
+     apiKey: "AIzaSyC8PAeOIs4Tf5qGLj_d4DzC1D6Z5AEw5yA",
+  authDomain: "newreffer-dc7f3.firebaseapp.com",
+  projectId: "newreffer-dc7f3",
+  storageBucket: "newreffer-dc7f3.firebasestorage.app",
+  messagingSenderId: "60384156805",
+  appId: "1:60384156805:web:8be9b4d64d15082dcc1dec",
+  measurementId: "G-X5F80G5R07"
 };
 
 // Initialize Firebase
